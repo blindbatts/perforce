c8ed2265e66a702dc67b971f7eca99abb4e8d906
blindbatts <blindbatts@gmail.com>
2021-06-24 15:04:32 -0700

    Update p4verify.sh

--- p4verify.sh
+++ p4verify.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # Variables
-adminmail=itreports@zindagigames.com
+adminmail=edit@me.com
 subject="Perforce: Weekly depot verification results"
 remote=/media/qnap/
 local=/perforce
 log=$local/scripts/p4verify.log
 pid=$$
+
+echo "`date +%x-%R` - $pid - Starting p4d -xv any inconsistancies found will follow:" >> $log
+	p4d -r $P4ROOT -xv >> $log
 
 echo "`date +%x-%R` - $pid - Starting P4Verify any inconsistancies found will follow:" >> $log
 
